5884d6df89d8386803344ab0bf3ef80e8b0318df
Stav Bernaz <stavbernaz@gmail.com>
2020-04-19 13:52:03 +0300

    version b

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/', function (req, res) {
-  console.log('{ version: a}');
-  res.send('{ version: a}');
+  console.log('{ version: b}');
+  res.send('{ version: b}');
 });
 
 // error handling
